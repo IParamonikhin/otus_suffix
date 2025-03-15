e172e4ab24e853ef38e24cc5ed41bf055b263998
IParamonikhin <paramonikhin.ivan@yandex.ru>
2025-03-15 12:54:51 +0300

    Initial Commit

---
+++ otus_suffix/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  otus_suffix
+//
+//  Created by Иван on 15.03.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ otus_suffix/otus_suffixApp.swift
@@ -0,0 +1,17 @@
+//
+//  otus_suffixApp.swift
+//  otus_suffix
+//
+//  Created by Иван on 15.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct otus_suffixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
